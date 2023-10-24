89c35a5ae5c1e5d1425f721bbe7bcd676c1b80a2
Letícia de Lima da Cunha <leticia.lc2005@aluno.ifsc.edu.br>
2023-10-23 21:23:34 -0300

    começando

---
+++ main.js
@@ -0,0 +1,16 @@
+let cifra = document.getElementById("idCifra").value
+let button = document.getElementById("idButton")
+let alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let out = document.getElementById("idOut")
+
+button.addEventListener("click", cifrarDecifrar)
+
+function cifrarDecifrar(){
+    let cifrarR = document.getElementById("input[name=RadioCifrar]:checked").value
+    if(cifrarR == "cifrar"){
+    
+    } else {
+        
+    }
+}
+
