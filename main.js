7d05ba1760395acae5d1edc6757f646903cedfcc
Letícia de Lima da Cunha <leticia.lc2005@aluno.ifsc.edu.br>
2023-10-25 23:12:09 -0300

    ajustando

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-let cifra = document.getElementById("idCifra").value
-let button = document.getElementById("idButton")
-let alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let out = document.getElementById("idOut")
-
-button.addEventListener("click", cifrarDecifrar)
-
-function cifrarDecifrar(){
-    let cifrarR = document.getElementById("input[name=RadioCifrar]:checked").value
-    if(cifrarR == "cifrar"){
+function principal() {
+    const mensagem = document.getElementById("mensagem").value;
+    const operacao = document.querySelector('input[name=operacao]:checked').value;
+    const chaveInput = document.getElementById("chave").value;
+    const chave = analisarChaveCifra(chaveInput);
     
+    const resultado = executarCifraCesar(mensagem, chave, operacao);
+    
+    document.getElementById("saida").value = resultado;
+}
+
+function analisarChaveCifra(chaveInput) {
+    if (isNaN(chaveInput)) {
+        return chaveInput.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0) + 1;
     } else {
-        
+        return parseInt(chaveInput);
     }
 }
+
+function executarCifraCesar(mensagem, chave, operacao) {
+    const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+    return mensagem.replace(/[a-zA-Z]/g, function (char) {
+        let base = char === char.toLowerCase() ? alfabeto : alfabeto.toLowerCase();
+        let index = base.indexOf(char);
+        
+        if (operacao === 'cifrar') {
+            index = (index + chave) % base.length;
+        } else if (operacao === 'decifrar') {
+            index = (index - chave + base.length) % base.length;
+        }
+        
+        return base[index];
+    });
+}
+
+document.getElementById("botaoExecutar").addEventListener("click", principal);
 
